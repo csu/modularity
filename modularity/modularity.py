797b6e9beb5d8c4d254a07c9738aa06e13744604
Christopher Su <christophersu9@gmail.com>
2015-11-16 20:35:30 -0800

    recursion

--- modularity/modularity.py
+++ modularity/modularity.py
@@ -2,8 +2,16 @@ import os
 
 def get_modules(modules_dir):
     dirs = []
-    for f in os.listdir(modules_dir):
-        path = os.path.join(modules_dir, f)
-        if os.path.isdir(path):
-            dirs.append(path.replace('/', '.'))
+
+    # we accept a list of directories:
+    if isinstance(lst, modules_dir):
+        for d in modules_dir:
+            dirs = dirs + get_modules(d)
+    # we're dealing with a single directory:
+    else:
+        for f in os.listdir(modules_dir):
+            path = os.path.join(modules_dir, f)
+            if os.path.isdir(path):
+                dirs.append(path.replace('/', '.'))
+
     return dirs
