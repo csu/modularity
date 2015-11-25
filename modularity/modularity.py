e4cdae10909d4461a5b3d00b740294fb6e632562
Christopher Su <christophersu9@gmail.com>
2015-11-24 21:29:50 -0800

    importing

--- modularity/modularity.py
+++ modularity/modularity.py
@@ -1,12 +1,14 @@
 import os
+from importlib import import_module
 
 def get_modules(modules_dir):
     dirs = []
 
     # we accept a list of directories:
-    if isinstance(lst, modules_dir):
+    if isinstance(modules_dir, list):
         for d in modules_dir:
             dirs = dirs + get_modules(d)
+
     # we're dealing with a single directory:
     else:
         for f in os.listdir(modules_dir):
@@ -15,3 +17,9 @@ def get_modules(modules_dir):
                 dirs.append(path.replace('/', '.'))
 
     return dirs
+
+def get_imported(modules_dir):
+    mods = []
+    for m in get_modules(modules_dir):
+        mods.append(import_module(m))
+    return mods
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README') as file:
 
 setup(
     name='modularity',
-    version='0.0.1',
+    version='0.0.2',
     description='A library for managing dynamic modules.',
     long_description=long_description,
     author='Christopher Su',
